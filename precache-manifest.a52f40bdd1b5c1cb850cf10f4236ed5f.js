ada729c9e626949180f3096083ecf3a87c9f3f9f
Olga <olga.kohanova@gmail.com>
2019-01-20 01:01:22 +0300

    Updates

---
+++ precache-manifest.a52f40bdd1b5c1cb850cf10f4236ed5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c256bc7b3ff1c6b2e8e",
+    "url": "/static/js/main.1c256bc7.chunk.js"
+  },
+  {
+    "revision": "92e65636ae49e23b06d8",
+    "url": "/static/js/1.92e65636.chunk.js"
+  },
+  {
+    "revision": "1c256bc7b3ff1c6b2e8e",
+    "url": "/static/css/main.8c8d1c4e.chunk.css"
+  },
+  {
+    "revision": "92e65636ae49e23b06d8",
+    "url": "/static/css/1.c9d0576d.chunk.css"
+  },
+  {
+    "revision": "36643381afd90b5ea865b8f6dce4f2ea",
+    "url": "/index.html"
+  }
+];
