c06630a27b929f9d81085a7cdef0a87b6a28f90e
Olga <olga.kohanova@gmail.com>
2019-01-20 00:48:27 +0300

    Updates

--- precache-manifest.a52f40bdd1b5c1cb850cf10f4236ed5f.js
+++ precache-manifest.a52f40bdd1b5c1cb850cf10f4236ed5f.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "f96dc319be653f3f1488",
-    "url": "/static/js/main.f96dc319.chunk.js"
+    "revision": "1c256bc7b3ff1c6b2e8e",
+    "url": "/static/js/main.1c256bc7.chunk.js"
   },
   {
     "revision": "92e65636ae49e23b06d8",
     "url": "/static/js/1.92e65636.chunk.js"
   },
   {
-    "revision": "f96dc319be653f3f1488",
-    "url": "/static/css/main.729a05bc.chunk.css"
+    "revision": "1c256bc7b3ff1c6b2e8e",
+    "url": "/static/css/main.8c8d1c4e.chunk.css"
   },
   {
     "revision": "92e65636ae49e23b06d8",
     "url": "/static/css/1.c9d0576d.chunk.css"
   },
   {
-    "revision": "ae06e726df4d41a1012b31b745e349f2",
+    "revision": "36643381afd90b5ea865b8f6dce4f2ea",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b1249d3c41e41771fcb146d11aad845.js"
+  "/precache-manifest.a52f40bdd1b5c1cb850cf10f4236ed5f.js"
 );
 
 workbox.clientsClaim();
