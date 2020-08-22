7132f23fd1937fb8d01c29f391be7e74cdb0478a
olgaaktas <binpolyglotte@gmail.com>
2020-08-23 01:03:03 +0300

    design improved

---
+++ precache-manifest.6600aa6329da8f4d760cd882f53ba24a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff02494d6fb2d3904d04",
+    "url": "/static/css/main.4d72da5e.chunk.css"
+  },
+  {
+    "revision": "ff02494d6fb2d3904d04",
+    "url": "/static/js/main.ff02494d.chunk.js"
+  },
+  {
+    "revision": "190acf523e3ed90dd368",
+    "url": "/static/css/1.0ce124d4.chunk.css"
+  },
+  {
+    "revision": "190acf523e3ed90dd368",
+    "url": "/static/js/1.190acf52.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b11f90336429c7c0c9ab8941d0ae3408",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fbd105c386ef8ae506560abf7403f9e2.js"
+  "/precache-manifest.6600aa6329da8f4d760cd882f53ba24a.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
